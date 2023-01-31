c890aaf8555557ee1a7a02d6f2fa4129fb16a299
AsyncAwaite <sunsei288@gmail.com>
2023-02-01 01:23:18 +0200

    upload 5

--- src/js/app.js
+++ src/js/app.js
@@ -38,7 +38,7 @@ function headerFixed() {
     if (header) {
         window.addEventListener("scroll", () => {
             try {
-                if (scrollY >= header.clientHeight ) {
+                if (scrollY >= header.clientHeight / 2 ) {
                     header.classList.add("--fixed");
                 } else {
                     header.classList.remove("--fixed");
